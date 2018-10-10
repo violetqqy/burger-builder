cde5199593f702c8bdd1431c86907118786bf29a
VioletQQY <forviolet771@gmail.com>
2018-10-10 14:57:01 +0800

    finish layout

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import classes from './SideDrawer.css'
+import Logo from '../../Logo/Logo'
+import NavigationItems from '../NavigationItems/NavigationItems'
+import Backdrop from '../../UI/Backdrop/Backdrop'
+import Aux from '../../../hoc/Aux'
+
+const sideDrawer = (props) => {
+  let attachdClasses = [classes.SideDrawer, classes.Close]
+  if (props.open) {
+    attachdClasses = [classes.SideDrawer, classes.Open]
+  }
+
+  return (
+    <Aux>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={attachdClasses.join(' ')}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </Aux>
+  ) 
+}
+
+export default sideDrawer
